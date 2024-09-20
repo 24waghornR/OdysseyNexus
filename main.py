f649dfa1c2f49bd8f3460e33c256419370ee4a57
Riley Waghorn <869riley@gmail.com>
2024-09-20 16:51:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import tkinter as tk
 
 mainWindow = tk.Tk()
+mainWindow.title("Odyssey Nexus - Project Manager")
+mainWindow.geometry(600x300)
+
+button = tk.Button(text="Close", action=self.quit)
+
+button.pack()
 mainWindow.mainloop()
